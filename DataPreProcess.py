2bb11b6daf5421226e52f3b4890e52e87a140229
Daniel <elezra73@gmail.com>
2019-05-21 13:25:22 +0300

    DataPreProcess is ready. GAF algorithm is now support

--- DataPreProcess.py
+++ DataPreProcess.py
@@ -1,9 +1,8 @@
 import numpy as np
 from mpl_toolkits.axes_grid1 import ImageGrid
 from pyts.image import GramianAngularField
-
+import math
 import pandas as pd
-import numpy
 import statistics
 # from pandas import Series
 import os
@@ -56,7 +55,20 @@ def CreateDataWithLinearInterpolation(CsvFilePath, PeriodOfTime, Deployment_Path
         # plt.axis('off')
         plt.plot(j, i)
         plt.axis('off')
-        plt.savefig(os.path.join(Deployment_Path, j[0] + ' ~ ' + j[PeriodOfTime - 1]))
+        Moves = 0
+        for x in range(PeriodOfTime):
+            try:
+                Moves = Moves + i[x + 1] - i[x]
+            except IndexError:
+                continue
+        try:
+            if Moves > 0:
+                plt.savefig(os.path.join(Deployment_Path, 'Up', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Up
+            else:
+                plt.savefig(os.path.join(Deployment_Path, 'Down', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Down
+        except IndexError:
+            break
+
         plt.close()
 
     # Close.plot(legend=None)
@@ -101,10 +113,8 @@ def CreateDataWithMovingAverageMapping(CsvFilePath, PeriodOfTime, Deployment_Pat
 
         # if statistics.mean(Close_Tuples[cnt+1]) > statistics.mean(Close_Tuples[cnt]):  #TODO Another way for decide Up/Down
         if Moves > 0:
-            print('Up')
             plt.savefig(os.path.join(Deployment_Path, 'Up', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Up
         else:
-            print('Down')
             plt.savefig(os.path.join(Deployment_Path, 'Down', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Down
 
         plt.close()
@@ -154,10 +164,8 @@ def CreateDataWithDoubleMovingAverageMapping(CsvFilePath, PeriodOfTime, Deployme
                 continue
 
         if Moves > 0:
-            print('Up')
             plt.savefig(os.path.join(Deployment_Path, 'Up', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Up
         else:
-            print('Down')
             plt.savefig(os.path.join(Deployment_Path, 'Down', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Down
 
         plt.close()
@@ -165,11 +173,10 @@ def CreateDataWithDoubleMovingAverageMapping(CsvFilePath, PeriodOfTime, Deployme
 
 
 # *****Main Function: this function get full path of .csv file, Method transformation and Period of time for the pictures range*****#
-def CreateDataWithGramianAngularField(CsvFilePath, PeriodOfTime, GAF_Path):
+def CreateDataWithGramianAngularField(CsvFilePath, PeriodOfTime, Deployment_Path):
     Close = pd.read_csv(CsvFilePath, usecols=["Close"])
     Open = pd.read_csv(CsvFilePath, usecols=["Open"])
     Date = pd.read_csv(CsvFilePath, usecols=["Date"])
-    cnt = 0
     Open_List = []
     Close_List = []
     Data_List = []
@@ -186,58 +193,73 @@ def CreateDataWithGramianAngularField(CsvFilePath, PeriodOfTime, GAF_Path):
     Open_Tuples = tuple(Open_List[i: i + PeriodOfTime] for i in range(0, len(Open), PeriodOfTime - 1))
     Close_Tuples = tuple(Close_List[i: i + PeriodOfTime] for i in range(0, len(Close), PeriodOfTime - 1))
     Date_Tuples = tuple(Data_List[i: i + PeriodOfTime] for i in range(0, len(Date), PeriodOfTime - 1))
-    NormelizedData = []
-
-
-
-
+    NormalizedData = []
+    OriginalData = []
     for i, j, k in zip(Close_Tuples, Date_Tuples, Open_Tuples):
-
         for x in range(PeriodOfTime):
             try:
-                min = np.min(i)
-                max = np.max(i)
+                minClose = np.min(i)
+                maxClose = np.max(i)
                 min_s = np.min(k)
                 max_s = np.max(k)
-                t = ((i[x] - max) + (i[x] - min)) / (max - min)
-                s = ((k[x] - max_s) + (k[x] - min_s)) / (max_s - min_s)
-                listT = []
-                listT.append(t)
-                #NormelizedData.append((t,j[x]))
-                NormelizedData.append(listT)
-
+                t = ((i[x] - maxClose) + (i[x] - minClose)) / (maxClose - minClose)     #close
+                s = ((k[x] - max_s) + (k[x] - min_s)) / (max_s - min_s) #open
+                NormalizedData.append(t)
+                OriginalData.append(i[x])
             except IndexError:
                 continue
-        print(NormelizedData)
+        gaf = GAF()
+        g, _, _, _ = gaf(NormalizedData)
+        plt.imshow(g, cmap='rainbow', origin='lower')
+        plt.axis('off')
+        #plt.show()
+        Moves = 0
+        for z in range(PeriodOfTime):  # TODO Another way for decide Up/Down
+            try:
+                Moves = Moves + OriginalData[z + 1] - OriginalData[z]
+            except IndexError:
+                continue
+
+        try:
+            if Moves > 0:
+                plt.savefig(os.path.join(Deployment_Path, 'Up', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Up
+            else:
+                plt.savefig(os.path.join(Deployment_Path, 'Down', j[0] + ' ~ ' + j[PeriodOfTime - 1] + '.jpg'))  # Down
+        except IndexError:
+            break
+        NormalizedData.clear()
+        OriginalData.clear()
+        plt.close()
 
 
-        gasf = GramianAngularField( method='summation')
-        X_gasf = gasf.fit_transform(NormelizedData, y=PeriodOfTime)
-        gadf = GramianAngularField(method='difference')
-        X_gadf = gadf.fit_transform(NormelizedData)
-        cnt += 1
+
+        # gasf = GramianAngularField( method='summation')
+        # X_gasf = gasf.fit_transform(NormalizedData, y=PeriodOfTime)
+        # gadf = GramianAngularField(method='difference')
+        # X_gadf = gadf.fit_transform(NormalizedData)
+        # cnt += 1
 
 
         # Show the images for the first time series
-        fig = plt.figure(figsize=(12, 7))
-        grid = ImageGrid(fig, 111,
-                     nrows_ncols=(1, 2),
-                     axes_pad=0.15,
-                     share_all=True,
-                     cbar_location="right",
-                     cbar_mode="single",
-                     cbar_size="7%",
-                     cbar_pad=0.3,
-                     )
-        images = [X_gasf[0] , X_gadf[0]]
-        titles = ['Gramian Angular Summation Field',
-              'Gramian Angular Difference Field']
-        for image, title, ax in zip(images, titles, grid):
-            im = ax.imshow(image, cmap='rainbow', origin='lower')
-            ax.set_title(title)
-        ax.cax.colorbar(im)
-        ax.cax.toggle_label(True)
-        plt.show()
+        # fig = plt.figure(figsize=(12, 7))
+        # grid = ImageGrid(fig, 111,
+        #              nrows_ncols=(1, 2),
+        #              axes_pad=0.15,
+        #              share_all=True,
+        #              cbar_location="right",
+        #              cbar_mode="single",
+        #              cbar_size="7%",
+        #              cbar_pad=0.3,
+        #              )
+        # images = [X_gasf[0] , X_gadf[0]]
+        # titles = ['Gramian Angular Summation Field',
+        #       'Gramian Angular Difference Field']
+        # for image, title, ax in zip(images, titles, grid):
+        #     im = ax.imshow(image, cmap='rainbow', origin='lower')
+        #     ax.set_title(title)
+        # ax.cax.colorbar(im)
+        # ax.cax.toggle_label(True)
+        # plt.show()
 
     return
 
@@ -245,11 +267,12 @@ def CreateDataWithGramianAngularField(CsvFilePath, PeriodOfTime, GAF_Path):
 def Main(CsvFileName, Method, PeriodOfTime):
     CsvFilePath = CsvFileName[: len(CsvFileName) - 4:]
     Deployment_Path = Create_Deployment(CsvFilePath)
-    print(Deployment_Path)
 
     if Method == 'Linear Interpolation':
         Linear_Path = os.path.join(Deployment_Path, 'Linear Interpolation')
         Create_Deployment(Linear_Path)
+        Create_Deployment(os.path.join(Linear_Path, 'Up'))
+        Create_Deployment(os.path.join(Linear_Path, 'Down'))
         CreateDataWithLinearInterpolation(CsvFileName, PeriodOfTime, Linear_Path)
 
     if Method == 'Moving Average Mapping' or Method == 'MAM':
@@ -274,9 +297,49 @@ def Main(CsvFileName, Method, PeriodOfTime):
         CreateDataWithGramianAngularField(CsvFileName, PeriodOfTime, GAF_Path)
 
 
+
+
+
+def tabulate(x, y, f):
+    """Return a table of f(x, y). Useful for the Gram-like operations."""
+    return np.vectorize(f)(*np.meshgrid(x, y, sparse=True))
+def cos_sum(a, b):
+    """To work with tabulate."""
+    return(math.cos(a+b))
+
+class GAF:
+
+    def __init__(self):
+        pass
+    def __call__(self, serie):
+        """Compute the Gramian Angular Field of an image"""
+        # Min-Max scaling
+        min_ = np.amin(serie)
+        max_ = np.amax(serie)
+        scaled_serie = (2*serie - max_ - min_)/(max_ - min_)
+
+        # Floating point inaccuracy!
+        scaled_serie = np.where(scaled_serie >= 1., 1., scaled_serie)
+        scaled_serie = np.where(scaled_serie <= -1., -1., scaled_serie)
+
+        # Polar encoding
+        phi = np.arccos(scaled_serie)
+        # Note! The computation of r is not necessary
+        r = np.linspace(0, 1, len(scaled_serie))
+
+        # GAF Computation (every term of the matrix)
+        gaf = tabulate(phi, phi, cos_sum)
+
+        return(gaf, phi, r, scaled_serie)
+
 #Main("Apple Stock Daily - 1.1.2012 ~ 2.4.2019.csv", 'MAM', 10)
 #Main("Elbit Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'MAM',10)
 #Main("Elbit Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'DMAM',10)
-Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'GAF',10)
+
+Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'Linear Interpolation',20)
+Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'MAM',20)
+Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'DMAM',20)
+Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'GAF',20)
+
 #Main("Intel Stock Daily - 1.1.2012 _ 2.4.2019.csv", 'MAM',10)
 # Main("Apple Stock Daily - 1.1.2012 ~ 2.4.2019.csv", 'Linear Interpolation',10)      #TODO get this value as input from user
--- Keras_Test.py
+++ Keras_Test.py
@@ -1,16 +1,80 @@
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
+from fastai.vision import *
+import random
 import numpy as np
+import matplotlib.pyplot as plt
+#from pyts.image import GramianAngularField
+#from pyts.image import GASF, GADF
+from pyts.image import gaf
+import pandas as pd
+# Math
+import math
+import numpy as np
+from datetime import datetime, timedelta
+import scipy
 
-model = VGG16(weights='imagenet', include_top=False)
+bs = 64
+n_classes = 3
+n_samples = 28
 
-img_path = 'C:\\Users\Daniel\Desktop\Linear_Interpolation\dogi.png'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+def tabulate(x, y, f):
+    """Return a table of f(x, y). Useful for the Gram-like operations."""
+    return np.vectorize(f)(*np.meshgrid(x, y, sparse=True))
+def cos_sum(a, b):
+    """To work with tabulate."""
+    return(math.cos(a+b))
 
-features = model.predict(x)
+class GAF:
 
-print(features)
+    def __init__(self):
+        pass
+    def __call__(self, serie):
+        """Compute the Gramian Angular Field of an image"""
+        # Min-Max scaling
+        min_ = np.amin(serie)
+        max_ = np.amax(serie)
+        scaled_serie = (2*serie - max_ - min_)/(max_ - min_)
+
+        # Floating point inaccuracy!
+        scaled_serie = np.where(scaled_serie >= 1., 1., scaled_serie)
+        scaled_serie = np.where(scaled_serie <= -1., -1., scaled_serie)
+
+        # Polar encoding
+        phi = np.arccos(scaled_serie)
+        # Note! The computation of r is not necessary
+        r = np.linspace(0, 1, len(scaled_serie))
+
+        # GAF Computation (every term of the matrix)
+        gaf = tabulate(phi, phi, cos_sum)
+
+        return(gaf, phi, r, scaled_serie)
+
+
+
+
+
+def begin():
+        while 1:
+            points = []
+            for i in range(20):
+                points.append(random.uniform(-1, 1))
+
+
+            X = np.linspace(0, 1, num=n_samples) ** 2
+            Y = [-1.0, -0.5169489478597231, -0.27118697931651753, -0.39830567724816807, -0.21186576773973353, -0.11016996193642421, 0.06779536770990188, 0.025423315857337772, -0.4915269031893659, -0.5762710068944942, -0.279662406636613, -0.07627248994597155, 0.5593209997122872, 0.8389821351619227, 1.0, 1.0, 0.8728813020683495, 0.8559321423441296, 0.864406722206238, 0.593220166618717]
+
+            gaf = GAF()
+            print(Y)
+            g, _, _, _ = gaf(Y)
+            plt.figure(figsize=(16, 8))
+            plt.subplot(121)
+            plt.plot(Y)
+            plt.title("Original", fontsize=16)
+            plt.subplot(122)
+            plt.axis('off')
+            plt.imshow(g, cmap='rainbow', origin='lower')
+            plt.title("GAF", fontsize=16)
+            plt.show()
+            points.clear()
+            plt.close()
+
+begin()
